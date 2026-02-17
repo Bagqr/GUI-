050ff9c71c78910a056244a592369f21dd01d80d
Богдан Авдюков <maksnervy666@gmail.com>
2026-02-17 20:19:49 +0700

    file and redact points made

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Documents;
 using System.IO;
@@ -24,7 +23,7 @@ namespace WpfApp1
 
         private void save_file_Click(object sender, RoutedEventArgs e)
         {
-            System.Windows.Forms.SaveFileDialog sfd = new System.Windows.Forms.SaveFileDialog();
+            SaveFileDialog sfd = new SaveFileDialog();
             sfd.Filter = "Text files(*.txt)|*.txt|C# files (*.cs)|*.cs|Xaml files (*.xaml)|*.xaml|All files (*.*)|*.*";
             sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             //sfd.InitialDirectory = @"c:\";
@@ -43,6 +42,40 @@ namespace WpfApp1
                 File.WriteAllText(FileLocation, tr.Text);
             }
         }
+        private void save_as_file_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text files(*.txt)|*.txt|C# files (*.cs)|*.cs|Xaml files (*.xaml)|*.xaml|All files (*.*)|*.*";
+            sfd.InitialDirectory = @"c:\";
+            TextRange tr = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, tr.Text);
+            }
+        }
 
+        private void open_file_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            TextRange tr = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tr.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+        private void new_file_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange tr = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            tr.Text = "";
+            FileLocation = "";
+        }
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Undo();
+        }
+        private void front_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Redo();
+        }
     }
 }
